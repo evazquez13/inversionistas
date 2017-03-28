6de0ba1a5818b35092ddef24bf182057107a3ff8
Matt Foster <aortizv@ids.com.mx>
2017-03-28 15:14:39 -0600

    Se agrego el codigo del banner al archivo page-index y al custom de la carpeta css

--- page-index.php
+++ page-index.php
@@ -13,35 +13,69 @@
 
 get_header(); ?>
 
-<?php 
-	$img = get_field('row-img-1');
-	$descripcion = get_field('descripcion');
-	$urlBoton = get_field('url-boton');
-?>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+	<?php
+	$i == 0;
+		// check if the repeater field has rows of data
+		if( have_rows('banner') ):
+			// loop through the rows of data
+			while ( have_rows('banner') ) : the_row();
+			?>
+    
+		    <div class="item <?php if($i == 0) {echo 'active';} ?>">
 
-<?php 
-	// Start the loop.
-	while ( have_posts() ) : the_post();
-    // Ruta de la imagen destacada (tamaño completo)
-    global $post;
-    $thumbID = get_post_thumbnail_id( $post->ID );
-    $imgDestacada = wp_get_attachment_url( $thumbID );    	
-?>
-	
-	<div>
-		<div class="container" style="background:url('<?php echo $img; ?>'); background-size: cover;">
-			<div class="row">
-				<div class="col-md-6">
-					<p><?php echo $descripcion; ?></p>
-					<a href="<?php echo $urlBoton; ?>"><button type="button" class="btn btn-primary"> 
-					Consultar</button></a>
-					<?php
-					the_content();
-					endwhile;
-					?>
-				</div>
-			</div>
-		</div>
-	</div>
+		      <img src="<?php the_sub_field('imagen'); ?>">
+		      <div class="carousel-caption">
+		            <div class="ctxt banner-msn">
+						<div class="row">
+							<div class="col-md-7 col-sm-7 col-xs-7">
+								<h2><?php the_sub_field('titulo'); ?></h2>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-7 col-sm-7 col-xs-7">
+								<p><?php the_sub_field('introduccion'); ?></p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-7 col-sm-7 col-xs-7 text-left">
+								<a href="<?php the_sub_field('boton'); ?>"><button type="button" class="btn btn-info">Saber más</button></a>
+							</div>
+						</div>
+		    		</div>
+		      </div>
+		    </div>
+
+	<?php 
+		$i ++;
+		endwhile;
+		endif;
+	?>
+
+ </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+   	<?php for ($j=0; $j < $i ; $j++) { ?>
+
+   			<li data-target="#carousel-example-generic" data-slide-to="<?php echo $j; ?>" <?php if ($j == 0) {echo " class='active' ";} ?> ></li> 
+
+	<?php } ?>
+  </ol>
+
+</div>
+
 <?php get_footer(); ?>
