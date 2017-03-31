05d40ee2e31d8f47ab65fe53a802e72bc7d2659d
Matt Foster <aortizv@ids.com.mx>
2017-03-31 11:25:15 -0600

    Se agrego al header.php el tema de la pag, se le dio estilo en custom.css

--- header.php
+++ header.php
@@ -27,11 +27,14 @@
   <div class="fonfoAzul">    
     <div class="container cabecera">
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
           <div class="logo-container">
             <a class="navbar-trigger">&nbsp;</a>  
             <a href="/" class="logo-cabecera"><img src="http://ec2-52-50-46-136.eu-west-1.compute.amazonaws.com/wp-content/uploads/2017/03/group-2.png" height="90" class="img-responsive hidden-xs" src="#"></a>
           </div>
+        </div>
+        <div class="col-md-4 accionistahead">
+            <p><span class="bordehead"></span>Accionistas e Inversores</p>
         </div>
       </div>
     </div>
