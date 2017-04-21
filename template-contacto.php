f823484743e95e53f6d2eed147902a47878229e1
Matt Foster <aortizv@ids.com.mx>
2017-04-21 14:08:50 -0500

    Se elimino archivo duplicado, se arreglo template-contacto para sus campos personalizados

--- template-contacto.php
+++ template-contacto.php
@@ -9,17 +9,7 @@ get_header();
 	$img = get_field('imag-1');
 	$titulo = get_field('titulo-1');
 	$intro = get_field('introduccion-1');
-	$boton = get_field('boton-1');
-	$url = get_field('url-boton-1');
-	$icon = get_field('icon-1');
-	$text = get_field('text-icon-1');
-	$campo = get_field('campo-html');
 	$contacto = get_field('contacto');
-	$img2 = get_field('imag-2');
-	$titulo2 = get_field('titulo-2');
-	$intro2 = get_field('introduccion-2');
-	$boton2 = get_field('boton-2');
-	$url2 = get_field('url-boton-2');
 	$idioma = pll_current_language( 'slug' );
 ?>
 
@@ -36,25 +26,6 @@ get_header();
 					<div class="col-md-10 col-sm-10 col-xs-11">
 						<p><?php echo $intro; ?></p>
 					</div>
-					<div class="col-md-12">
-						<?php if($url !=''){ ?>
-						<div class="col-md-6 col-sm-5 col-xs-12">
-							<a href="<?php echo $url; ?>"><button type="button" class="btn btn-primary"><?php echo $boton; ?></button></a>
-						</div>
-						<?php } 
-						if($icon !=''){ ?>
-						<div class="col-md-6 col-sm-7 col-xs-12">
-							<a href="#">
-							 <div class="col-md-3 col-sm-1 col-xs-2">
-							 	<img src="<?php echo $icon; ?>">
-							 </div>
-							 <div class="col-md-8 col-sm-6 col-xs-9">
-								<?php echo $text; ?>
-						   	 </div>
-							</a>
-						</div>
-						<?php } ?>
-					</div>
 				</div>
 			</div>
 			<?php } ?>
@@ -62,19 +33,11 @@ get_header();
 	</div>
 </div>
 
-<?php } 
-if($campo !=''){?>
-
-<?php the_field('campo-html'); ?>
-
-<?php } ?>
-
 <?php 
 if($contacto !=''){ 
 	?>
 
 <div class="space"></div>
-
 <div>
 	<div class="container">
 		<div class="row">
@@ -89,9 +52,7 @@ if($contacto !=''){
 		</div>
 	</div>
 </div>
-
 <div class="space"></div>
-
 <div>
 	<div class="container">
 		<div class="row">
@@ -139,33 +100,6 @@ if($contacto !=''){
 <?php } ?>
 
 <div class="space"></div>
-
-<?php if($img2 !=''){ ?>
-<div style="background:url('<?php echo $img2; ?>'); background-size: cover; height: 360px">
-	<div class="container contacto">
-		<div class="row">
-		<?php if($titulo2 !=''){ ?>
-			<div class="col-md-6 col-sm-6 col-xs-11">
-				<div class="bloque">
-					<div class="col-md-10 col-sm-10 col-xs-11">
-						<h1><?php echo $titulo2; ?></h1>	
-					</div>
-					<div class="col-md-10 col-sm-10 col-xs-11">
-						<p><?php echo $intro2; ?></p>
-					</div>
-					<?php if($url2 !=''){ ?>
-					<div class="col-md-6 col-sm-6 col-xs-6">
-						<a href="<?php echo $url2; ?>"><button type="button" class="btn btn-primary"><?php echo $boton2; ?></button></a>
-					</div>
-					<?php } ?>
-				</div>
-			</div>
-			<?php } ?>
-		</div>
-	</div>
-</div>
-<?php }
-?>
 
 <?php if (get_field ('interes')) {?>
 <div class="infoInteres">
